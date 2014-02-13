f0450b16b3c58b9e03b2a7b82644f1c893f8a805
Stefan May <smay@4finger.net>
2014-02-13 08:21:48 +0100

    added error handling for Twitter search

--- TweetSearch.pm
+++ TweetSearch.pm
@@ -16,13 +16,25 @@ my $nt = Net::Twitter->new(
 
 sub search {
     my $search_term = shift or die;
+    my @tweets = ();
 
-    my $result = $nt->search($search_term, { since_id => ($persistent->{since_id} or 0)});
-    $persistent->{since_id} = $result->{search_metadata}->{max_id};
+    eval {
+        my $result = $nt->search($search_term, { since_id => ($persistent->{since_id} or 0)});
 
-    store($persistent, 'persistent.dat');
+        $persistent->{since_id} = $result->{search_metadata}->{max_id};
+        store($persistent, 'persistent.dat');
 
-    my @tweets = sort { $a->{id} cmp $b->{id} } @{$result->{statuses}};
+        @tweets = sort { $a->{id} cmp $b->{id} } @{$result->{statuses}};
+    };
+    if ( $@ ) {
+        if ( blessed $@ && $@->isa('Net::Twitter::Error') ) {
+            warn "Twitter search failed: " . $@->error . "\n";
+        }
+        else {
+            # something bad happened!
+            die $@;
+        }
+    }
 
     return @tweets;
 }
